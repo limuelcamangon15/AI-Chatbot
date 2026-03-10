13c3f39c3d287923ecd3abf105ef242c5bc184bd
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-03-10 18:24:38 +0800

    update: import update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { prompt } from "./prompt";
+import { prompt } from "./prompt.js";
 
 dotenv.config();
 
